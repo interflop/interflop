b9009714ea244efd4d520c200ec76ad1ec2925c2
Pablo Oliveira <pablo@sifflez.org>
2017-06-19 14:52:04 +0200

    Add IEEE backend and simple build interface
    Interflop IEEE backend does the corresponding IEEE operation
    for each instrumentation hook. Therefore it should have no impact
    on the original program apart from making it slower.
    
    It includes an environment variable that allows to print a message
    for each call to the backend.

---
+++ include/interflop.h
@@ -0,0 +1,19 @@
+/* interflop backend interface */
+
+struct interflop_backend_interface_t {
+  void (*interflop_add_float)(float, float, float*, void*);
+  void (*interflop_sub_float)(float, float, float*, void*);
+  void (*interflop_mul_float)(float, float, float*, void*);
+  void (*interflop_div_float)(float, float, float*, void*);
+
+  void (*interflop_add_double)(double, double, double*, void*);
+  void (*interflop_sub_double)(double, double, double*, void*);
+  void (*interflop_mul_double)(double, double, double*, void*);
+  void (*interflop_div_double)(double, double, double*, void*);
+};
+
+/* interflop_init: called at initialization before using a backend.
+ * It returns an interflop_backend_interface_t structure with callbacks
+ * for each of the numerical instrument hooks */
+
+struct interflop_backend_interface_t interflop_init(void ** context);
